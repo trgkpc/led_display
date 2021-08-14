251574e7a73a3b8bd5927de9007bd37235300a65
Kentaro Seki <kentaro.kpc@gmail.com>
2021-08-14 15:44:20 +0900

    firmwareのaddressを自動生成へ

--- firmware/address/main.cpp
+++ firmware/address/main.cpp
@@ -1,33 +1,37 @@
+// 入出力機能
 #include <iostream>
+#include <fstream>
 #include <string>
+using std::cout, std::cerr, std::endl;
+using std::ofstream;
+using std::string, std::to_string;
 #define _SFR_IO8(id) (id)
-#define print(name) print_impl(#name, name)
-void print_impl(std::string name, int id)
+#define print(name) (cout << (#name) << " " <<(name) << endl);
+ofstream f;
+void define_led(string X, int ddr, int port, int id)
 {
-    std::cout  << name << " " << id << std::endl;
+    string name = "LED_" + X + to_string(id);
+    f << "#define" << " " << name << " ";
+    f << "LED<" << ddr << "," << port << "," << id << ">" << endl;
 }
-void define_led(std::string X, int ddr, int port, int id)
-{
-    std::string name = "LED" + X;
-    std::cout << "#define " << name << id << " ";
-    std::cout << "LED<" << ddr << "," << port << "," << id << ">" << std::endl;
-}
-void define_leds(std::string X, int ddr, int port)
+void define_leds(string X, int ddr, int port)
 {
     for(int i=0; i<8; i++){
         define_led(X, ddr, port, i);
     }
 }
-#define LEDX_IMPL(X) define_leds(#X, DDR##X,  PORT##X)
+#define LED(X) define_leds(#X, DDR##X, PORT##X)
 
+// ピン周りのファイル読み込み
 #define _AVR_IO_H_
 #include </usr/lib/avr/include/avr/iom328p.h>
 
 int main()
 {
-    //print(DDRD);
-    LEDX_IMPL(B);
-    LEDX_IMPL(C);
-    LEDX_IMPL(D);
-}
+    f = ofstream("led.hpp");
+    LED(B);
+    LED(C);
+    LED(D);
+    f.close();
 
+}
--- main.cpp
+++ main.cpp
@@ -7,22 +7,15 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+auto led_green = LED_D0();
+auto pwm = PWM0A();
+
 int main(void)
 {
-    DDRB = 0x00;
-    DDRC = 0x00;
-    DDRD = 0x01;
-
-    PORTB = 0xff;
-    PORTC = 0xff;
-    PORTD = 0xff;
-    
-    auto led1 = LEDD0();
-    PWM0A pwm = PWM0A();
     uint8_t t = 0;
 
     while(1){
-        led1.toggle();
+        led_green.toggle();
         pwm.set(t);
         _delay_ms(100);
         t += 25;
